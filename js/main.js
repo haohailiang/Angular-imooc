8eec91f22cd97943cd0b4b8b3931cdb994ceda96
haohailiang <646263409@qq.com>
2017-12-19 17:21:57 +0800

    自定义指令-错误
    比较2个密码是否相等-目前代码有bug,等待以后修正

--- js/main.js
+++ js/main.js
@@ -4,4 +4,22 @@ angular.module('myApp', []).controller('mainController', function($scope){
   $scope.submitSignup = function(){
     console.log($scope.userdata);
   }
+})
+.directive('compare', function(){
+  var o = {};
+  o.strict = 'AE';
+  o.scope = {
+    orgText: '=compare'
+  };
+  o.require = 'ngModel';
+  o.link = function(scope, element, attribule, con){
+    con.$validators.compare = function(value){
+      return value == scope.orgText;
+    }
+
+    scope.$watch('orgText', function(){
+      con.$validate();
+    })
+  }
+  return o;
 });
