e7897745d224f4f2fc19c8385629a23b07b57a1a
haohailiang <646263409@qq.com>
2017-12-19 17:04:04 +0800

    add

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-angular.module('myApp', []);
+angular.module('myApp', []).controller('mainController', function($scope){
+  $scope.userdata = {};
+
+  $scope.submitSignup = function(){
+    console.log($scope.userdata);
+  }
+});
